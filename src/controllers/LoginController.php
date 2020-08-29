77bdccfe1107497a500f3dd4bc071cddb1b8a4a3
CharlieCidral <69029099+CharlieCidral@users.noreply.github.com>
2020-08-29 18:25:48 -0300

    Add files via upload
    Ajuda com erro undefined

---
+++ src/controllers/LoginController.php
@@ -0,0 +1,74 @@
+<?php
+namespace src\controllers;
+
+use \core\Controller;
+use \src\handlers\LoginHandler;
+
+class LoginController extends Controller {
+
+    public function signin() {
+        $flash = '';
+        if(!empty($_SESSION['flash'])) {
+            $flash = $_SESSION['flash'];
+            $_SESSION['flash'] = '';
+        }
+        $this->render('signin', [
+            'flash' => $flash
+        ]);
+    }
+
+    public function signinAction() {
+        $email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
+        $password = filter_input(INPUT_POST, 'password');
+
+        if($email && $password) {
+
+            $token = LoginHandler::verifyLogin($email, $password);
+            if($token) {
+                $_SESSION['token'] = $token;
+                $this->redirect('/');
+            } else {
+                $_SESSION['flash'] = 'E-mail e/ou senha não conferem.';
+                $this->redirect('/login');
+            }
+        } else {
+            $_SESSION['flash'] = 'Digite os campos de e-mail e/ou senha.';
+            $this->redirect('/login');
+        }
+    }
+
+    public function signup() {
+        $flash = '';
+        if(!empty($_SESSION['flash'])) {
+            $flash = $_SESSION['flash'];
+            $_SESSION['flash'] = '';
+        }
+        $this->render('signup', [
+            'flash' => $flash
+        ]);
+    }
+    
+    public function signupAction() {
+        $name = filter_input(INPUT_POST, 'name');
+        $email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
+        $password = filter_input(INPUT_POST, 'password');
+        $birthdate = filter_input(INPUT_POST, 'birthdate');
+
+        if($name && $email && $password) {
+
+
+            if(LoginHandler::emailExists($email) === false) {
+                $token = LoginHandler::addUser($name, $email, $password, $birthdate);
+                $_SESSION['token'] = $token;
+                $this->redirect('/');
+            } else {
+                $_SESSION['flash'] = 'E-mail já cadastrado!';
+                $this->redirect('/cadastro');
+            }
+            
+        } else {
+            $this->redirect('/cadastro');
+        }
+    }
+
+}
