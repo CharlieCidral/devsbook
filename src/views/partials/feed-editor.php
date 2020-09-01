f8e2235234a5bc1079d65178393734080a16f15c
Charlie Cidral <charliesamoel@gmail.com>
2020-09-01 09:49:42 -0300

    including post in the database and checking that the post is not empty

--- src/views/partials/feed-editor.php
+++ src/views/partials/feed-editor.php
@@ -21,7 +21,7 @@ let feedSubmit = document.querySelector('.feed-new-send');
 let feedForm = document.querySelector('.feed-new-form');
 
 feedSubmit.addEventListener('click', function(obj){
-    const value = feedInput.innerText;
+    const value = feedInput.innerText.trim();
 
     if(value != '') {
         feedForm.querySelector('input[name=body]').value = value;
