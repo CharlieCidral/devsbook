401955b3b3893ef613318f9306541a1185640d06
CharlieCidral <69029099+CharlieCidral@users.noreply.github.com>
2020-09-01 05:50:52 -0300

    Update feed-editor

--- src/views/partials/feed-editor.php
+++ src/views/partials/feed-editor.php
@@ -21,7 +21,7 @@ let feedSubmit = document.querySelector('.feed-new-send');
 let feedForm = document.querySelector('.feed-new-form');
 
 feedSubmit.addEventListener('click', function(obj){
-    let value = feedInput.InnerText;
+    const value = feedInput.innerText;
 
     if(value != '') {
         feedForm.querySelector('input[name=body]').value = value;
