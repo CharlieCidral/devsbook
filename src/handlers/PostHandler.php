f82866e0af6806202e4174a4a304a0272f151f9c
Charlie Cidral <charliesamoel@gmail.com>
2020-09-02 05:04:13 -0300

    width of the photo-item, helper, getfeed, homefeed and updating the ProfileController

--- src/handlers/PostHandler.php
+++ src/handlers/PostHandler.php
@@ -18,6 +18,65 @@ class PostHandler {
             ])->execute();
         }
     }
+
+    public function _postListToObject($postList, $loggedUserId) {
+        $posts = [];
+        foreach($postList as $postItem) {
+            $newPost = new Post();
+            $newPost->id = $postItem['id'];
+            $newPost->type = $postItem['type'];
+            $newPost->created_at = $postItem['created_at'];
+            $newPost->body = $postItem['body'];
+            $newPost->mine = false;
+
+            if($postItem['id_user'] == $loggedUserId) {
+                $newPost->mine = true;
+            }
+
+            $newUser = User::select()->where('id', $postItem['id_user'])->one();
+            $newPost->user = new User();
+            $newPost->user->id = $newUser['id'];
+            $newPost->user->name = $newUser['name'];
+            $newPost->user->avatar = $newUser['avatar'];
+
+            $newPost->likeCount = 0;
+            $newPost->liked = false;
+
+            $newPost->comments = [];
+
+            $posts[] = $newPost;
+
+        }
+
+        return $posts;
+
+    }
+
+    public static function getUserFeed($idUser, $page, $loggedUserId) {
+        $perPage = 2;
+
+        $postList = Post::select()
+            ->where('id_user', $idUser)
+            ->orderBy('created_at', 'desc')
+            ->page($page, $perPage)
+        ->get();
+
+        $total = Post::select()
+            ->where('id_user', $idUser)
+        ->count();
+
+        $pageCount = ceil($total / $perPage);
+
+        $posts = self::_postListToObject($postList, $idUser);
+
+        return [
+            'posts' => $posts,
+            'pageCount' => $pageCount,
+            'currentPage' => $page
+        ];
+        
+    }
+
     public static function getHomeFeed($idUser, $page) {
         $perPage = 2;
 
@@ -41,32 +100,7 @@ class PostHandler {
 
         $pageCount = ceil($total / $perPage);
 
-        $posts = [];
-        foreach($postList as $postItem) {
-            $newPost = new Post();
-            $newPost->id = $postItem['id'];
-            $newPost->type = $postItem['type'];
-            $newPost->created_at = $postItem['created_at'];
-            $newPost->body = $postItem['body'];
-            $newPost->mine = false;
-
-            if($postItem['id_user'] == $idUser) {
-                $newPost->mine = true;
-            }
-
-            $newUser = User::select()->where('id', $postItem['id_user'])->one();
-            $newPost->user = new User();
-            $newPost->user->id = $newUser['id'];
-            $newPost->user->name = $newUser['name'];
-            $newPost->user->avatar = $newUser['avatar'];
-
-            $newPost->likeCount = 0;
-            $newPost->liked = false;
-
-            $newPost->comments = [];
-
-            $posts[] = $newPost;
-        }
+        $posts = self::_postListToObject($postList, $idUser);
 
         return [
             'posts' => $posts,
@@ -87,7 +121,7 @@ class PostHandler {
         foreach($photosData as $photo) {
             $newPost = new Post();
             $newPost->id = $photo['id'];
-            $newPost->type = $photo['photo'];
+            $newPost->type = $photo['type'];
             $newPost->created_at = $photo['created_at'];
             $newPost->body = $photo['body'];
 
