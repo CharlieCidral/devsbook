60c42b3cbc09f27a43dfb99804cf4a142d0afbc1
Charlie Cidral <charliesamoel@gmail.com>
2020-09-04 04:56:20 -0300

    button follow, function isFollowing and render

--- src/controllers/ProfileController.php
+++ src/controllers/ProfileController.php
@@ -21,33 +21,31 @@ class ProfileController extends Controller {
         $page = intval(filter_input(INPUT_GET, 'page'));
 
         $id = $this->loggedUser->id;
-
         if(!empty($atts['id'])){
             $id = $atts['id'];
         }
-
         $user = UserHandler::getUser($id, true);
-
         if(!$user) {
             $this->redirect('/');
         }
-
         $dateFrom = new \DateTime($user->birthdate);
         $dateTo = new \DateTime('today');
-
         $user->ageYears = $dateFrom->diff($dateTo)->y;
-
         $feed = PostHandler::getUserFeed(
             $id,
              $page, 
              $this->loggedUser->id
         );
-
-
+        
+        $isFollowing = false;
+        if($user->id != $this->loggedUser->$id) {
+            $isFollowing = UserHandler::$isFollowing($this->loggedUser->$id, $user->id);
+        }
         $this->render('profile',[
             'loggedUser' =>$this->loggedUser,
             'user' => $user,
-            'feed' => $feed
+            'feed' => $feed,
+            'isFollowing' => $isFollowing
         ]);
 
     }
