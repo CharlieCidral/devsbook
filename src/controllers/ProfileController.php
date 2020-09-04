fef601e0cd9c31ffcb61d2eaa1239655fad7c945
Charlie Cidral <charliesamoel@gmail.com>
2020-09-04 08:00:08 -0300

    routes from profile_friends and created profile_friends

--- src/controllers/ProfileController.php
+++ src/controllers/ProfileController.php
@@ -68,4 +68,30 @@ class ProfileController extends Controller {
 
     }
 
+    public function friends() {
+        $id = $this->loggedUser->id;
+        if(!empty($atts['id'])){
+            $id = $atts['id'];
+        }
+        $user = UserHandler::getUser($id, true);
+        if(!$user) {
+            $this->redirect('/');
+        }
+        $dateFrom = new \DateTime($user->birthdate);
+        $dateTo = new \DateTime('today');
+        $user->ageYears = $dateFrom->diff($dateTo)->y;
+
+        $isFollowing = false;
+        if($user->id != $this->loggedUser->id) {
+            $isFollowing = UserHandler::isFollowing($this->loggedUser->id, $user->id);
+        }
+
+        $this->render('profile_friends',[
+            'loggedUser' =>$this->loggedUser,
+            'user' => $user,
+            'isFollowing' => $isFollowing
+        ]);
+
+    }
+
 }
